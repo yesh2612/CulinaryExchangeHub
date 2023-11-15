c4de9402f692bb7384d2cafdfeed6479ae7ebbd1
Subash <zubhazh@gmail.com>
2023-11-15 13:26:54 -0800

    Modified searchstyle javascript code to display proper dish output

--- static/searchstyle.js
+++ static/searchstyle.js
@@ -1,24 +1,127 @@
-const recipes = [
-    { title: 'Pasta Carbonara', ingredients: ['pasta', 'eggs', 'bacon', 'parmesan'] },
-    { title: 'Chicken Curry', ingredients: ['chicken', 'curry sauce', 'rice'] },
-    { title: 'Vegetarian Pizza', ingredients: ['dough', 'tomato sauce', 'cheese', 'vegetables'] }
-];
+var xhr = new XMLHttpRequest();
+let value;
+xhr.open("POST", "/searchstyle", true);
 
-function searchRecipe() {
-    const searchTerm = document.getElementById('recipeTitle').value.toLowerCase();
-    const searchResults = recipes.filter(recipe => recipe.title.toLowerCase().includes(searchTerm));
+xhr.setRequestHeader("Content-Type", "application/json");
 
-    const resultsContainer = document.getElementById('searchResults');
-    resultsContainer.innerHTML = '';
+xhr.onload = function () {
+  if (xhr.status >= 200 && xhr.status < 300) {
+    var jsonData = JSON.parse(xhr.responseText);
+    value = jsonData;
+  } else {
+    console.error(
+      "Error fetching data. Status:",
+      xhr.status,
+      "Text:",
+      xhr.statusText
+    );
+  }
+};
 
-    if (searchResults.length === 0) {
-        resultsContainer.innerHTML = '<p>No results found</p>';
-    } else {
-        searchResults.forEach(recipe => {
-            const recipeDiv = document.createElement('div');
-            recipeDiv.classList.add('recipe');
-            recipeDiv.innerHTML = `<h3>${recipe.title}</h3><p>Ingredients: ${recipe.ingredients.join(', ')}</p>`;
-            resultsContainer.appendChild(recipeDiv);
-        });
+xhr.onerror = function () {
+  console.error("Network error while fetching data.");
+};
+
+xhr.send(JSON.stringify({}));
+
+function searchValue(data) {
+  var check_dish_exist_flag = false;
+  data_array = data.data;
+  const searchTerm = document.getElementById("recipeTitle").value.toLowerCase();
+  const resultsContainer = document.getElementById("searchResults");
+  resultsContainer.innerHTML = "";
+  if (searchTerm.length === 0) {
+    console.log("null");
+    resultsContainer.innerHTML = "<p>No results found</p>";
+  } else if (searchTerm.length != 0) {
+    console.log("not null");
+    const recipeDiv = document.createElement("div");
+    recipeDiv.classList.add("recipe");
+
+    for (let i = 0; i < data_array.length; i++) {
+      const item = data_array[i];
+
+      if (item[0].toLowerCase() === searchTerm.toLowerCase()) {
+        check_dish_exist_flag = true;
+        var dishLabel = createLabel("Dish Name: ");
+        var dishElement = createElementWithStyle(
+          "span",
+          "labelElement",
+          item[0]
+        );
+
+        var authorLabel = createLabel("Author Name: ");
+        var authorElement = createElementWithStyle(
+          "span",
+          "labelElement",
+          item[1]
+        );
+
+        var ingredientsLabel = createLabel("Ingredients: ");
+        var ingredientsElement = createElementWithStyle(
+          "span",
+          "labelElement",
+          item[2]
+        );
+
+        var stepsLabel = createLabel("Steps to Prepare: ");
+        var stepsElement = createElementWithStyle(
+          "span",
+          "labelElement",
+          item[3]
+        );
+
+        recipeDiv.appendChild(dishLabel);
+        recipeDiv.appendChild(dishElement);
+        recipeDiv.appendChild(document.createElement("br"));
+
+        recipeDiv.appendChild(authorLabel);
+        recipeDiv.appendChild(authorElement);
+        recipeDiv.appendChild(document.createElement("br"));
+
+        recipeDiv.appendChild(ingredientsLabel);
+        recipeDiv.appendChild(ingredientsElement);
+
+        recipeDiv.appendChild(document.createElement("br"));
+
+        recipeDiv.appendChild(stepsLabel);
+        recipeDiv.appendChild(stepsElement);
+        resultsContainer.appendChild(recipeDiv);
+        add_style(recipeDiv);
+        break;
+      }
     }
+    if (check_dish_exist_flag === false) {
+      resultsContainer.innerHTML = "<p>No results found</p>";
+    }
+  }
+}
+
+function createLabel(text) {
+  var label = document.createElement("label");
+  label.textContent = text;
+  label.style.display = "inline-block";
+  label.style.width = "120px";
+  label.style.marginBottom = "5px";
+  return label;
+}
+
+function createElementWithStyle(tag, className, text) {
+  var element = document.createElement(tag);
+  element.textContent = text;
+  element.className = className;
+  return element;
+}
+
+function add_style(element) {
+  element.style.border = "1px solid #ccc";
+  element.style.padding = "20px";
+  element.style.margin = "20px";
+  element.style.height = "auto";
+  element.style.borderRadius = "8px";
+  element.style.backgroundColor = "rgb(67, 161, 233)";
+  element.style.boxShadow = "0 0 10px rgb(59, 106, 166)";
+}
+function searchRecipe() {
+  searchValue(value);
 }
