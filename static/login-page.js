a051d9084f51e7e97ed1eef92ec9ab7c1190d1df
Subash <zubhazh@gmail.com>
2023-11-26 19:08:19 -0800

    Added forgot password feature where when user clicks the forgot password he will be able to set a new one. Validated the login functionality and worte clear_form which will clear the entries in the text when user navigates to forgot password

--- static/login-page.js
+++ static/login-page.js
@@ -2,6 +2,35 @@ var form = document.forms.myform;
 
 var validation_flag = true;
 
+function make_fields_defaut_color(field, color) {
+  document.getElementById(field).style.color = color;
+  document.getElementById(field).style.borderColor = color;
+}
+function clear_entries(field) {
+  document.getElementById(field).value = "";
+}
+function clear_error_fields(field) {
+  document.getElementById(field).textContent = "";
+}
+function set_default_class(field) {
+  document.getElementById(field).className = "login-form-field";
+}
+function clear_form() {
+  make_fields_defaut_color("main-holder", "black");
+  document.getElementById("main-holder").style.backgroundColor = "white";
+  make_fields_defaut_color("login-error-msg", "black");
+  make_fields_defaut_color("email", "black");
+  make_fields_defaut_color("password", "black");
+  make_fields_defaut_color("forgot-email", "black");
+  make_fields_defaut_color("new-password", "black");
+  make_fields_defaut_color("confirm-password", "black");
+  clear_error_fields("login-error-msg");
+  clear_entries("email");
+  clear_entries("password");
+  clear_entries("forgot-email");
+  clear_entries("new-password");
+  clear_entries("confirm-password");
+}
 function display_user_not_found_msg(xmlhttp) {
   console.error("Request failed with status", xmlhttp.status);
   document.getElementById("main-holder").style.backgroundColor = "white";
@@ -35,7 +64,6 @@ function validate_user_email(user_email) {
   console.log(emailRegex.test(email_value));
   if (emailRegex.test(email_value)) {
     document.getElementById("email").style.borderColor = "black";
-    // validation_flag = true;
     return 1;
   } else {
     document.getElementById("email").style.borderColor = "red";
@@ -44,27 +72,124 @@ function validate_user_email(user_email) {
     document.getElementById("login-error-msg").style.color = "red";
     document.getElementById("login-error-msg").textContent =
       "Incorrect Email ID";
-    // validation_flag = false;
     return 0;
   }
 }
 
 function validate_user_password(user_password) {
-  if (user_password === "") {
-    document.getElementById("password").style.borderColor = "red";
-    document.getElementById("password").className = "error-field";
-    // validation_flag = false;
+  var user_pwd = user_password.value;
+  if (user_pwd === "") {
+    user_password.style.borderColor = "red";
+    user_password.className = "error-field";
     return 0;
-  } else if (user_password != "") {
+  } else if (user_pwd != "") {
     document.getElementById("main-holder").style.backgroundColor = "white";
-    document.getElementById("password").style.borderColor = "black";
-    // validation_flag = true;
+    user_password.className = "login-form-field";
+    user_password.style.borderColor = "black";
     return 1;
   }
 }
 
+function toggleForm(formType) {
+  clear_form();
+  set_default_class("email");
+  set_default_class("password");
+  set_default_class("forgot-email");
+  set_default_class("new-password");
+  set_default_class("confirm-password");
+  console.log("wwwwwwwwwwwwwwwwwww", formType);
+  var loginForm = document.getElementById("login-form");
+  var forgotPasswordForm = document.getElementById("forgot-password-form");
+
+  if (formType === "forgot-password") {
+    loginForm.style.display = "none";
+    forgotPasswordForm.style.display = "block";
+  } else {
+    loginForm.style.display = "block";
+    forgotPasswordForm.style.display = "none";
+  }
+}
+
+function validate_user_confirm_password(user_password, confirm_password) {
+  console.log("passsword", user_password, confirm_password);
+  var user_pwd = user_password.value;
+  var confirm_pwd = confirm_password.value;
+  if (user_pwd != confirm_pwd || confirm_pwd === "") {
+    confirm_password.style.borderColor = "red";
+    confirm_password.className = "error-field";
+    user_password.style.borderColor = "red";
+    user_password.className = "error-field";
+    document.getElementById("login-error-msg").textContent =
+      "Password Mismatch";
+    document.getElementById("login-error-msg").style.color = "red";
+    return 0;
+  } else {
+    confirm_password.className = "login-form-field";
+    return 1;
+  }
+}
+
+function resetPassword() {
+  var email_id = document.getElementById("forgot-email");
+  var new_password = document.getElementById("new-password");
+  var confirm_password = document.getElementById("confirm-password");
+  if (validate_user_email(email_id) === 0) {
+    return;
+  }
+  if (validate_user_password(new_password) === 0) {
+    // document.getElementById("new-password").style.borderColor = "red";
+    // document.getElementById("new-password").className = "error-field";
+    return;
+  }
+  if (validate_user_confirm_password(new_password, confirm_password) === 0) {
+    return;
+  }
+  document.getElementById("login-error-msg").textContent = "";
+  document.getElementById("login-error-msg").style.color = "black";
+  // if (new_password.value !== confirm_password.value) {
+  //   alert("Passwords do not match. Please try again.");
+  //   return;
+  // }
+
+  var xhr = new XMLHttpRequest();
+
+  xhr.open("POST", "/password_reset", true);
+  xhr.setRequestHeader("Content-Type", "application/json");
+
+  xhr.onload = function () {
+    if (xhr.status >= 200 && xhr.status < 300) {
+      var response = JSON.parse(xhr.responseText);
+      console.log(response);
+
+      if (response.success) {
+        document.getElementById("main-holder").style.backgroundColor =
+          "#00ff99";
+        document.getElementById("login-error-msg").style.borderColor = "black";
+        document.getElementById("login-error-msg").textContent =
+          "New Password set Successfully";
+        document.getElementById("login-error-msg").style.color = "black";
+
+        // toggleForm("login");
+      } else {
+        alert("Failed to reset password. Please try again.");
+      }
+    } else {
+      console.error("Request failed with status " + xhr.status);
+      alert("Error resetting password. Please try again.");
+    }
+  };
+
+  var data = JSON.stringify({
+    email_id: email_id.value,
+    new_password: new_password.value,
+  });
+
+  xhr.send(data);
+}
+
 function url() {
-  var user_password = document.getElementById("password").value;
+  clear_form();
+  var user_password = document.getElementById("password");
   var email_input = document.getElementById("email");
   var email_id_value = document.getElementById("email").value;
 
@@ -83,12 +208,12 @@ function url() {
   document.getElementById("email").className = "login-form-field";
   document.getElementById("password").className = "login-form-field";
 
-  console.log(email_id_value, user_password);
+  console.log(email_id_value, user_password.value);
   var xmlhttp = new XMLHttpRequest();
 
   const data = {
     email_id: email_id_value,
-    password: user_password,
+    password: user_password.value,
   };
   xmlhttp.open("POST", "/login", true);
   xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
